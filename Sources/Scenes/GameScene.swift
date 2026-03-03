7725c6508f8efe5ae5d090d8152d431563bc379b
jacopo <poggiodellesca@gmail.com>
2026-03-03 20:48:49 +0100

    Try different voice ID

--- Sources/Scenes/GameScene.swift
+++ Sources/Scenes/GameScene.swift
@@ -371,7 +371,7 @@ class GameScene: SKScene {
         print("🎤 TTS Request (disabled): \(text)")
         
         let apiKey = "sk_f0fb6161f1d1a2426d1e67c4fcff341b3e95d5380db2e3fa"
-        let voiceId = "VR6AewTLgFxGq8SpwX53"  // Sam voice (common)
+        let voiceId = "iP95pCZ7a4qJpS0jAEJP"  // Try another voice
         
         guard let url = URL(string: "https://api.elevenlabs.io/v1/text-to-speech/\(voiceId)") else { return }
         
